945babc5ef16a6badac17f3ae7828dfffb1bbe94
Brody <brodeonus65@gmail.com>
2023-10-05 23:46:29 -0400

    dragon update

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,25 @@
-// Your code here
+class Dragon{
 
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+    this.dragons = []
+    Dragon.getDragons(name)
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(dragon) {
+    this.dragons.push(this.name)
+    // console.log(this.dragons)
+    return this.dragons;
+  }
+
+}
+// let dragon1 = new Dragon("Trogdor", "Black")
+// let dragon2 = new Dragon("Charizard", "Orange")
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
